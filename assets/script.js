d97a081e67d0b6805350dacf318823aea121c552
FuzzyGiraffee <121840200+FuzzyGiraffee@users.noreply.github.com>
2023-02-21 09:34:38 -0600

    button now toggles data state

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,16 @@
 //selects quiz button and stores as variable
 var submitEl = document.querySelector(".quizButton");
 var timeEl = document.querySelector(".timer")
+var quizEl = document.querySelector(".quizContent")
 
 //Default timer max length
 var timeLeft = 60
 
-
-
 function test (event) {
-    
-var element = event.target
-    //Toggles Hidden Status
-    if (hiddenStatus= "visable"){
-        element.setAttribute("hiddenStatus", "hidden")
-        console.log(hiddenStatus)
-        //TODO: currently returns
-    }else if (hiddenstatus = "hidden") {
-        element.setAttribute("hiddenStatus", "visable")
-        console.log(hiddenStatus)
-    }
+
+var element = event.target;
+var state = element.getAttribute("data-state");
+
     //timer
     var timerInterval = setInterval(function() {
         timeLeft--;
@@ -29,10 +21,17 @@ var element = event.target
         }
     }, 1000);
 
-
-
-    
+    //Toggles Hidden Status
+    if (state == "visible"){
+        element.setAttribute("data-state", "hidden")
+        quizEl.replaceAll()
+    }else  {
+        element.setAttribute("data-state", "visible")
+    }
+    state = element.getAttribute("data-state");
+    console.log(state)
 }
+
 
 
 //event listener for button click
